d88800da7ee417d418a11e7e8e1590026d83946c
Julien Bisconti <julien.bisconti@gmail.com>
2016-11-27 13:12:08 +0700

    add clojurescript REPL still not working

--- src/cljs/swagger_service/core.cljs
+++ src/cljs/swagger_service/core.cljs
@@ -1,94 +1,64 @@
+
 (ns swagger-service.core
-  (:require [reagent.core :as r]
-            [reagent.session :as session]
-            [secretary.core :as secretary :include-macros true]
-            [goog.events :as events]
-            [goog.history.EventType :as HistoryEventType]
-            [markdown.core :refer [md->html]]
-            [swagger-service.ajax :refer [load-interceptors!]]
-            [ajax.core :refer [GET POST]])
-  (:import goog.History))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [ajax.core :refer [GET]])
+  (:require-macros [secretary.core :refer [defroute]]))
 
-(defn nav-link [uri title page collapsed?]
-  [:li.nav-item
-   {:class (when (= page (session/get :page)) "active")}
-   [:a.nav-link
-    {:href uri
-     :on-click #(reset! collapsed? true)} title]])
+(defn fetch-links! [links link-count]
+  (GET "/api/cat-links"
+       {:params {:link-count link-count}
+        :handler #(reset! links (vec (partition-all 6 %)))}))
 
-(defn navbar []
-  (let [collapsed? (r/atom true)]
-    (fn []
-      [:nav.navbar.navbar-light.bg-faded
-       [:button.navbar-toggler.hidden-sm-up
-        {:on-click #(swap! collapsed? not)} "☰"]
-       [:div.collapse.navbar-toggleable-xs
-        (when-not @collapsed? {:class "in"})
-        [:a.navbar-brand {:href "#/"} "swagger-service"]
-        [:ul.nav.navbar-nav
-         [nav-link "#/" "Home" :home collapsed?]
-         [nav-link "#/about" "About" :about collapsed?]]]])))
 
-(defn about-page []
-  [:div.container
-   [:div.row
-    [:div.col-md-12
-     "this is the story of swagger-service... work in progress"]]])
+(defn images [links]
+  [:div.text-xs-center
+    (for [row (partition-all 3 links)] ^{:key row}
+      [:div.row
+        (for [link row]
+          ^{:key link}
+          [:div.col-sm-4 [:img {:width 400 :src link}]])])])
+
+(defn forward [i pages]
+  (if (< i (dec pages)) (inc i) i))
+
+(defn back [i]
+  (if (pos? i) (dec i) i))
+
+(defn nav-link [page i]
+  [:li.page-item>a.page-link.btn.btn-primary
+    {:on-click #(reset! page i)
+     :class   (when (= i @page) "active")}
+    [:span i]])
+
+(defn pager [pages page]
+  (when (> pages 1)
+    (into
+      [:div.text-xs-center>ul.pagination.pagination-lg]
+      (concat
+        [[:li.page-item>a.page-link.btn
+          {:on-click #(swap! page back pages)
+           :class (when (= @page 0) "disabled")}
+          [:span "«"]]]
+        (map (partial nav-link page) (range pages))
+        [[:li.page-item>a.page-link.btn
+          {:on-click #(swap! page forward pages)
+           :class (when (= @page (dec pages)) "disabled")}
+          [:span "»"]]]))))
+
 
 (defn home-page []
-  [:div.container
-   [:div.jumbotron
-    [:h1 "Welcome to swagger-service"]
-    [:p "Time to start building your site!"]
-    [:p [:a.btn.btn-primary.btn-lg {:href "http://luminusweb.net"} "Learn more »"]]]
-   [:div.row
-    [:div.col-md-12
-     [:h2 "Welcome to ClojureScript"]]]
-   (when-let [docs (session/get :docs)]
-     [:div.row
-      [:div.col-md-12
-       [:div {:dangerouslySetInnerHTML
-              {:__html (md->html docs)}}]]])])
-
-(def pages
-  {:home #'home-page
-   :about #'about-page})
-
-(defn page []
-  [(pages (session/get :page))])
-
-;; -------------------------
-;; Routes
-(secretary/set-config! :prefix "#")
-
-(secretary/defroute "/" []
-  (session/put! :page :home))
-
-(secretary/defroute "/about" []
-  (session/put! :page :about))
-
-;; -------------------------
-;; History
-;; must be called after routes have been defined
-(defn hook-browser-navigation! []
-  (doto (History.)
-        (events/listen
-          HistoryEventType/NAVIGATE
-          (fn [event]
-              (secretary/dispatch! (.-token event))))
-        (.setEnabled true)))
-
-;; -------------------------
-;; Initialize app
-(defn fetch-docs! []
-  (GET (str js/context "/docs") {:handler #(session/put! :docs %)}))
+  (let [page (atom 0)
+        links (atom nil)]
+    (fetch-links! links 50)
+    (fn []
+      (if (not-empty @links)
+        [:div.container>div.row>div.col-md-12
+          [pager (count @links) page]
+          [images (@links @page)]]
+        [:div "Standby for cats!"]))))
 
 (defn mount-components []
-  (r/render [#'navbar] (.getElementById js/document "navbar"))
-  (r/render [#'page] (.getElementById js/document "app")))
+  (reagent/render-component [home-page] (.getElementById js/document "app")))
 
 (defn init! []
-  (load-interceptors!)
-  (fetch-docs!)
-  (hook-browser-navigation!)
   (mount-components))
